bcc7edcea5cfacab32546ee0ffb04fdd9e4e4e5c
setiawanboedy <gonisetiawan0@gmail.com>
2022-08-10 18:22:53 +0800

    portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['index.html'],
+  content: ['index.html', 'public/js/script.js'],
   theme: {
     container: {
       center: true,
